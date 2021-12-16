0b80eda4f67c21b32eca09cf17132480fe5eee61
Amethyst Willia <zmithic@gmail.com>
2021-12-16 13:59:26 -0600

    Installed react-router-dom and added a page, let's see if this works

--- vercel-app/src/components/App.js
+++ vercel-app/src/components/App.js
@@ -1,9 +1,35 @@
+import React from 'react';
 import './App.css';
+import { Route, NavLink } from "react-router-dom";
+import CharacterCard from './CharacterCard';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <header>
+        <h1>DanganRonpa V3 Cast</h1>
+        <nav>
+          <NavLink to='/kaede-akamatsu'>Kaede Akamatsu</NavLink>
+          <NavLink to='/shuichi-saihara'>Shuichi Saihara</NavLink>
+          <NavLink to='/miu-iruma'>Miu Iruma</NavLink>
+          <NavLink to='/angie-yonaga'>Angie Yonaga</NavLink>
+          <NavLink to='/gonta-gokuhara'>Gonta Gokuhara</NavLink>
+          <NavLink to='/kokichi-ouma'>Kokichi Ouma</NavLink>
+          <NavLink to='/k1-b0'>K1-B0</NavLink>
+          <NavLink to='/kaito-momota'>Kaito Momota</NavLink>
+          <NavLink to='/maki-harukawa'>Maki Harukawa</NavLink>
+          <NavLink to='/ryoma-hoshi'>Ryoma Hoshi</NavLink>
+          <NavLink to='/tenko-chabashira'>Tenko Chabashira</NavLink>
+          <NavLink to='/himiko-yumeno'>Himiko Yumeno</NavLink>
+        </nav>
+      </header>
+      <div>
+        <h2>This is my trash fandom</h2>
+        <p>Sorry not sorry.</p>
+      </div>
+      <Route path='/kaede-akamatsu'>
+        <CharacterCard/>
+      </Route>
     </div>
   );
 }
---
+++ vercel-app/src/components/CharacterCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const CharacterCard = () => {
+    return (
+        <div className='char-card'>
+            <header className='char-select'>
+                <nav>
+                    <a href='#'>Thing go here</a>
+                </nav>
+                <h1>Character Name</h1>
+                <h2>Ultimate Talent</h2>
+            </header>
+            <div className='char-info'>
+                <div className='char-traits'>
+                    <div className='physical-traits'>
+                    <ul>
+                        <li>Height:</li>
+                        <li>Weight:</li>
+                        <li>Gender:</li>
+                        <li>Blood Type:</li>
+                    </ul>
+                    </div>
+                    <div className='other-traits'>
+                    <ul>
+                        <li>Date of Birth:</li>
+                        <li>Likes:</li>
+                        <li>Dislikes:</li>
+                        <li>Quote:</li>
+                    </ul>
+                    </div>
+                </div>
+                <div className='char-bio'>
+                    <div className='backstory'>
+                        Blah blah blah
+                    </div>
+                    <div className='triva'>
+                        <ul>
+                            <li>Thing!</li>
+                            <li>Thing!</li>
+                            <li>Thing!</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='char-pic'>
+                    <img src='https://upload.wikimedia.org/wikipedia/en/9/95/Test_image.jpg'/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CharacterCard;
